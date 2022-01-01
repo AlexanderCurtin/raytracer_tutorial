e5305d3718923d777ef5298069bcbfb9b4a8f78e
Alexander Curtin <a.curtin@elsevier.com>
2021-12-31 20:45:32 -0500

    Finished Section 6

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,99 @@
 use std::{
-    fmt::Write,
     io::{stdout, BufWriter, Write as IoWrite},
     ops::{Add, AddAssign, Div, DivAssign, Index, Mul, MulAssign, Neg, Sub, SubAssign},
+    rc::Rc,
 };
+
+const INFINITY: f64 = f64::INFINITY;
+const pi: f64 = 3.1415926535891932384626;
+
+pub fn degrees_to_radians(degrees: f64) -> f64 {
+    degrees * pi / 180.0
+}
 
 #[derive(Clone, Copy)]
 pub struct Vec3 {
     x: f64,
     y: f64,
     z: f64,
+}
+
+pub struct HitRecord {
+    p: Point3,
+    normal: Vec3,
+    t: f64,
+    front_face: bool,
+}
+
+impl HitRecord {
+    fn set_face_normal(&mut self, r: &Ray, outward_normal: &Vec3) {
+        self.front_face = dot(&r.direction, &outward_normal) < 0.0;
+        self.normal = if self.front_face {
+            *outward_normal
+        } else {
+            -*outward_normal
+        };
+    }
+}
+
+pub trait Hit {
+    fn hit(&self, ray: &Ray, t_min: f64, t_max: f64) -> Option<HitRecord>;
+}
+
+struct Sphere {
+    center: Point3,
+    radius: f64,
+}
+
+impl Hit for Vec<Rc<dyn Hit>> {
+    fn hit(&self, ray: &Ray, t_min: f64, t_max: f64) -> Option<HitRecord> {
+        let mut rec = None as Option<HitRecord>;
+        let mut closest_so_far = t_max;
+        for o in self.iter() {
+            if let Some(temp_rec) = o.hit(ray, t_min, closest_so_far) {
+                closest_so_far = temp_rec.t;
+                rec = Some(temp_rec);
+            }
+        }
+        return rec;
+    }
+}
+
+impl Hit for Sphere {
+    fn hit(&self, r: &Ray, t_min: f64, t_max: f64) -> Option<HitRecord> {
+        let oc = r.origin - self.center;
+        let a = r.direction.length_squared();
+        let half_b = dot(&oc, &r.direction);
+        let c = oc.length_squared() - self.radius * self.radius;
+
+        let discriminant = half_b * half_b - a * c;
+        if discriminant < 0.0 {
+            return None;
+        }
+
+        let sqrtd = discriminant.sqrt();
+
+        let mut root = (-half_b - sqrtd) / a;
+        if (root < t_min || t_max < root) {
+            root = (-half_b + sqrtd) / a;
+            if (root < t_min || t_max < root) {
+                return None;
+            }
+        }
+
+        let p = r.at(root);
+        let mut rec = HitRecord {
+            p: r.at(root),
+            normal: (p - self.center) / self.radius,
+            t: root,
+            front_face: false,
+        };
+
+        let outward_normal = (rec.p - self.center) / self.radius;
+        rec.set_face_normal(r, &outward_normal);
+
+        Some(rec)
+    }
 }
 
 type Point3 = Vec3;
@@ -168,7 +253,7 @@ impl DivAssign<f64> for Vec3 {
     }
 }
 
-struct Ray {
+pub struct Ray {
     origin: Point3,
     direction: Vec3,
 }
@@ -177,69 +262,43 @@ impl Ray {
     fn at(&self, t: f64) -> Vec3 {
         return self.origin + t * self.direction;
     }
-    fn color(&self) -> Color {
-        const SPHERE_CENTER: Point3 = Point3 {
-            x: 0.,
-            y: 0.,
-            z: -1.,
-        };
-        let t = hit_sphere(&SPHERE_CENTER, 0.5, &self);
-        if t > 0.0 {
-            let N = unit_vector(
-                &(self.at(t)
-                    - Vec3 {
-                        x: 0.,
-                        y: 0.,
-                        z: -1.,
-                    }),
-            );
+    fn color(&self, world: &dyn Hit) -> Color {
+        if let Some(rec) = world.hit(self, 0., INFINITY) {
             return 0.5
-                * Color {
-                    x: N.x + 1.,
-                    y: N.y + 1.,
-                    z: N.z + 1.,
-                };
+                * (rec.normal
+                    + Color {
+                        x: 1.,
+                        y: 1.,
+                        z: 1.,
+                    });
         }
         let unit_direction = unit_vector(&self.direction);
         let t = 0.5 * (unit_direction.y + 1.);
-        let _left = (1. - t)
+        return (1. - t)
             * Color {
                 x: 1.,
                 y: 1.,
                 z: 1.,
-            };
-        let _right: Vec3 = t * Color {
-            x: 0.5,
-            y: 0.7,
-            z: 1.,
-        };
-        let _ans = _left + _right;
-        let real_ans = ((1. - t)
-            * Color {
-                x: 1.,
-                y: 1.,
-                z: 1.,
-            })
+            }
             + t * Color {
                 x: 0.5,
                 y: 0.7,
-                z: 1.,
+                z: 1.0,
             };
-        return real_ans;
     }
 }
 
 fn hit_sphere(center: &Point3, radius: f64, r: &Ray) -> f64 {
     let oc = r.origin - *center;
-    let a = dot(&r.direction, &r.direction);
-    let b = 2.0 * dot(&oc, &r.direction);
-    let c = dot(&oc, &oc) - radius * radius;
-    let discriminant = b * b - 4.0 * a * c;
+    let a = r.direction.length_squared();
+    let half_b = dot(&oc, &r.direction);
+    let c = oc.length_squared() - radius * radius;
+    let discriminant = half_b * half_b - a * c;
 
     if discriminant < 0. {
         return -1.0;
     };
-    (-b - discriminant.sqrt()) / (2.0 * a)
+    (-half_b - discriminant.sqrt()) / a
 }
 
 fn main() {
@@ -247,6 +306,27 @@ fn main() {
     const ASPECT_RATIO: f64 = 16.0 / 9.0;
     const IMAGE_WIDTH: usize = 400;
     const IMAGE_HEIGHT: usize = (IMAGE_WIDTH as f64 / ASPECT_RATIO) as usize;
+
+    // World
+    let mut world: Vec<Rc<dyn Hit>> = Vec::new();
+
+    world.push(Rc::new(Sphere {
+        center: Point3 {
+            x: 0.,
+            y: 0.,
+            z: -1.,
+        },
+        radius: 0.5,
+    }));
+
+    world.push(Rc::new(Sphere {
+        center: Point3 {
+            x: 0.,
+            y: -100.5,
+            z: -1.,
+        },
+        radius: 100.,
+    }));
 
     // Camera
 
@@ -291,7 +371,7 @@ fn main() {
                 origin,
                 direction: lower_left_corner + u * horizontal + v * vertical - origin,
             };
-            let pixel_color = r.color();
+            let pixel_color = r.color(&world);
             pixel_color.write_color(&mut writer);
         }
     }
